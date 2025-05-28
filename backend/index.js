1529d4efc57d775eaf5922d88edde9ba505175e6
hashim-i222478 <i222478@nu.edu.pk>
2025-05-28 13:57:56 +0500

    removed env

--- backend/index.js
+++ backend/index.js
@@ -49,5 +49,5 @@ app.use("/api/favorites", favoritesRoutes);
 app.use("/api/admin", manageReviews);
 app.use("/api/booking", analytics);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
